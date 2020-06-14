22c349562fa28c2fa3c12a021b0da4767daacc64
Nicholas Carrara <ncarrara@albany.edu>
2020-06-13 23:12:25 -0400

    CBLAS now does our matrix multiplications for us

--- src/main.cpp
+++ src/main.cpp
@@ -27,26 +27,6 @@ int main()
   ET::Matrix<double> mat7("G",2,3,{1,2,3,4,5,6});
   mat7.print();
 
-  /* Locals */
-    double A[5][3] = {1,1,1,2,3,4,3,5,2,4,2,5,5,4,3};
-    double b[5][2] = {-10,-3,12,14,14,12,16,16,18,16};
-    lapack_int info,m,n,lda,ldb,nrhs;
-
-    /* Initialization */
-    m = 5;
-    n = 3;
-    nrhs = 2;
-    lda = 3;
-    ldb = 2;
-
-
-
-    /* Executable statements */
-    printf( "LAPACKE_dgels (row-major, high-level) Example Program Results\n" );
-    /* Solve least squares problem*/
-    info = LAPACKE_dgels(LAPACK_ROW_MAJOR,'N',m,n,nrhs,*A,lda,*b,ldb);
-
-    /* Print Solution */
-    printf( "\n" );
+  
   return 0;
 }
